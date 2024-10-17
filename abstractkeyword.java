83cc1e1fe866e91b387a2c098449ff56d17dcdfe
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2024-10-17 19:35:11 +0530

    Create abstractkeyword.java

---
+++ abstractkeyword.java
@@ -0,0 +1,31 @@
+
+
+// Parent Class
+abstract class gfg {
+    abstract void printInfo();
+}
+
+// Child Class
+class employee extends gfg {
+    void printInfo()
+    {
+        String name = "aakanksha";
+        int age = 21;
+        float salary = 55552.2F;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
+
+// Driver Class
+class base {
+    // main function
+    public static void main(String args[])
+    {
+        // object created
+        gfg s = new employee();
+        s.printInfo();
+    }
+}
